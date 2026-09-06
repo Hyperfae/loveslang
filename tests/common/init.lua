5178160265a57db0e1f8c61b1e003ffd45266a80
hyperfae <hyperboid@proton.me>
2026-09-06 03:27:19 -0400

    Slightly nicer test thing

--- tests/common/init.lua
+++ tests/common/init.lua
@@ -1,14 +1,24 @@
 ---@class testcommon
 local testcommon = {}
 
-function testcommon.drawshader(shader)
+function testcommon.drawshader(shader, reflection)
     local canvas = love.graphics.newCanvas(320, 240)
+    canvas:setFilter("nearest")
     love.graphics.setCanvas(canvas)
     love.graphics.clear(1,1,1,1)
     love.graphics.setCanvas()
+    local time = 0.0
     function love.draw()
         love.graphics.setShader(shader)
-        love.graphics.draw(canvas)
+        if shader:hasUniform("globalParams_0.time_0") then
+            shader:send("globalParams_0.time_0", time)
+        end
+        local sw, sh = love.graphics.getDimensions()
+        love.graphics.draw(canvas, 0, 0, 0, sw / canvas:getWidth(), sh / canvas:getHeight())
+    end
+    function love.update(dt)
+        time = time + dt
+        time = time % 1
     end
 end
 
